5d7030cd1ac0267d1df50ebd47a400906f7ea07c
YG <yeraglez@hotmail.com>
2024-05-11 09:52:54 +0100

    Commit con JavaDoc

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -5,18 +5,38 @@
 package cuentas;
 
 /**
- *
- * @author MF-YG
- */
+ * La clase CCuenta representa una cuenta bancaria.
+ * Permite realizar operaciones como retirar e ingresar dinero,
+ * así como obtener el estado actual de la cuenta.
+ * 
+ * @author Yeray González Toledo
+ 
+ * @see <a href="https://docs.oracle.com/en/java/">JavaDcoumentation</a>”)
+*/
+
 public class CCuenta {
     
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
+    private String nombre;          // Nombre del titular de la cuenta
+    private String cuenta;          // Número de cuenta
+    private double saldo;           // Saldo actual de la cuenta
+    private double tipoInterés;     // Tipo de interés de la cuenta
     
+    /**
+    * Constructor por defecto de la clase CCuenta.
+    * Crea una instancia de CCuenta con valores predeterminados.
+    */
+
     public CCuenta()
     { }
+    
+    /**
+     * Constructor de la clase CCuenta.
+     * 
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Número de cuenta
+     * @param sal Saldo inicial de la cuenta
+     * @param tipo Tipo de interés de la cuenta
+     */
     
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
@@ -25,10 +45,22 @@ public class CCuenta {
         saldo=sal;
     }
     
+    /**
+     * Obtiene el estado actual de la cuenta.
+     * 
+     * @return El saldo actual de la cuenta
+     */
+    
     public double estado()
     {
         return getSaldo();
     }
+    
+    /**
+     * Retira una cantidad de dinero de la cuenta.
+     * 
+     * @param cantidad La cantidad a retirar
+     */
     
     public void retirar(double cantidad)
     {try {
@@ -42,6 +74,12 @@ public class CCuenta {
         }
     }
     
+    /**
+     * Ingresa una cantidad de dinero en la cuenta.
+     * 
+     * @param cantidad La cantidad a ingresar
+     */    
+    
     public void ingresar(double cantidad)
     {try {
         System.out.println("Ingreso en cuenta:" + cantidad);
@@ -53,57 +91,77 @@ public class CCuenta {
         } 
     }
 
+    // Getters y setters
+    
     /**
-     * @return the nombre
+     * Obtiene el nombre del titular de la cuenta.
+     * 
+     * @return El nombre del titular de la cuenta
      */
+    
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta.
+     * 
+     * @param nombre El nombre del titular de la cuenta
      */
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Obtiene el número de cuenta.
+     * 
+     * @return El número de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Establece el número de cuenta.
+     * 
+     * @param cuenta El número de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo actual de la cuenta.
+     * 
+     * @return El saldo actual de la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Establece el saldo actual de la cuenta.
+     * 
+     * @param saldo El saldo actual de la cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * Obtiene el tipo de interés de la cuenta.
+     * 
+     * @return El tipo de interés de la cuenta
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * Establece el tipo de interés de la cuenta.
+     * 
+     * @param tipoInterés El tipo de interés de la cuenta
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
